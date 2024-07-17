4fecb3e37d8ee262fff19b029cba47bb1f77065b
tritant <tritant@hotmail.com>
2024-07-17 08:25:57 +0200

    add one node with categories weight

--- creaprompt.py
+++ creaprompt.py
@@ -9,6 +9,8 @@ folder_path = os.path.join(script_directory, "csv" )
 folder_path_1 = os.path.join(script_directory, "csv1" )
 folder_path_2 = os.path.join(script_directory, "csv2" )
 folder_path_3 = os.path.join(script_directory, "csv3" )
+folder_path_4 = os.path.join(script_directory, "csv+weight" )
+
 
 def getfilename(folder):
     name = []
@@ -356,7 +358,92 @@ class CreaPrompt_3:
          return (
             final_values,
             seed,
-         )         
+         )
+
+class CreaPrompt_4:
+
+    RETURN_TYPES = (
+        "STRING",
+        "INT",
+    )
+    RETURN_NAMES = (
+        "prompt",
+        "seed",
+    )
+    FUNCTION = "create_prompt"
+    CATEGORY = "CreaPrompt"
+    
+    def __init__(self, seed=None):
+        self.rng = random.Random(seed)
+
+    @classmethod
+    def IS_CHANGED(cls):
+        return float("NaN")
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        required = {}
+        for filename in os.listdir(folder_path_4):
+              if filename.endswith(".csv"):
+                 file_path = os.path.join(folder_path_4, filename)
+                 lines = []
+                 with open(file_path, 'r', encoding='utf-8') as file:
+                    lines = file.readlines()
+                    required[filename [3:-4]] = (["disabled"] + ["🎲random"] + lines, {"default": "disabled"})
+                    required[filename [3:-4] + ": Weight"] = ("FLOAT", {"default": 1, "min": -3, "max": 3})
+        return {
+            "required": required,
+            "optional": {
+                "Prompt_count":("INT", {"default": 1, "min": 1, "max": 1000}),
+                "CreaPrompt_Collection": (["disabled"] + ["enabled"], {"default": "disabled"}),
+                "seed": ("INT", {"default": 0, "min": 0, "max": 1125899906842624}),
+            }
+        }    
+    def create_prompt(self, **kwargs):
+        name_of_files = getfilename(folder_path_4)
+        seed = kwargs.get("seed", 0)
+        prompts_count = kwargs.get("Prompt_count", 0)
+        concatenated_values = ""
+        prompt_value = ""
+        final_values = ""
+        values = []
+        values = [""] * len(name_of_files)
+        if kwargs.get("CreaPrompt_Collection", 0) == "enabled":
+          for c in range(prompts_count):  
+            prompt_value = select_random_line_from_collection()  
+            print(f"➡️CreaPrompt prompt: {prompt_value}")  
+            final_values += prompt_value + "\n" 
+            prompt_value = ""            
+          final_values = final_values.strip()  
+          print(f"➡️CreaPrompt Seed: {seed}")
+          return (
+            final_values,
+            seed,
+          )            
+        else:         
+         for c in range(prompts_count):
+           for i, filename in enumerate(name_of_files):
+              if kwargs.get(filename, 0) == "🎲random":
+                     values[i] = select_random_line_from_csv_file(filename, folder_path_4)
+                     if kwargs.get(filename + ": Weight", 0) != 0 and kwargs.get(filename + ": Weight", 0) != 1: 
+                        values[i] = f"({values[i]}:{kwargs.get(filename + ': Weight', 0):.1f})"
+              else:      
+                     values[i] = kwargs.get(filename, 0)
+                     values[i] = values[i].strip()
+                     if kwargs.get(filename + ": Weight", 0) != 0 and kwargs.get(filename + ": Weight", 0) != 1: 
+                          values[i] = f"({values[i]}:{kwargs.get(filename + ': Weight', 0):.1f})"
+           for value in values:
+              if value != "disabled":
+                     concatenated_values += value + ","
+           print(f"➡️CreaPrompt prompt: {concatenated_values [:-1]}")
+           final_values += concatenated_values [:-1] + "\n" 
+           concatenated_values = ""
+         final_values = final_values.strip()  
+         print(f"➡️CreaPrompt Seed: {seed}")
+         return (
+            final_values,
+            seed,
+         )                  
        
 class CreaPrompt_list:
 
@@ -404,6 +491,7 @@ NODE_CLASS_MAPPINGS = {
     "CreaPrompt_1": CreaPrompt_1,
     "CreaPrompt_2": CreaPrompt_2,
     "CreaPrompt_3": CreaPrompt_3,
+    "CreaPrompt_4": CreaPrompt_4,
     "CreaPrompt List": CreaPrompt_list,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -411,6 +499,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "CreaPrompt_1": "CreaPrompt node 1",
     "CreaPrompt_2": "CreaPrompt node 2",
     "CreaPrompt_3": "CreaPrompt node 3",
+    "CreaPrompt_4": "CreaPrompt node with weight",
     "CreaPrompt List": "CreaPrompt Multi Prompts",
     "CSL": "Comma Separated List",
 }
