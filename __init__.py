7a79845f18839f7d80c65589c09c21c88f5821fa
tritant <tritant@hotmail.com>
2025-06-04 08:40:36 +0200

    Add dynamic UI

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
 from .creaprompt import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+#from . import creaprompt_api  # 👈 force l'import du module, donc exécute les routes
+WEB_DIRECTORY = "./js"
+#__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
 
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
